914c8723b10964d8b4cd48eb82d9999b110a09d0
Jonathan Reed <jdreed@mit.edu>
2011-03-02 14:20:27 -0500

    Bump version number

--- TracZephyrPlugin/setup.py
+++ TracZephyrPlugin/setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='TracZephyrPlugin',
-    version='1.4',
+    version='1.4.1',
     author='Evan Broder',
     author_email='broder@mit.edu',
     description='Send a zephyr when a Trac ticket is created or updated',
