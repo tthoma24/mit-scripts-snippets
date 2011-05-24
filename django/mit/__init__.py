2a644ab1cfed458a1afe7d0277afdfefa63f6248
Alex Dehnert <adehnert@mit.edu>
2011-05-24 16:16:21 -0400

    Properly escape the zwrite command line
    Thanks to Anders for bringing this issue to my attention.

--- django/mit/__init__.py
+++ django/mit/__init__.py
@@ -1,11 +1,16 @@
+import subprocess
+
 from django.contrib.auth.middleware import RemoteUserMiddleware
 from django.contrib.auth.backends import RemoteUserBackend
 from django.contrib import auth
 from django.core.exceptions import ObjectDoesNotExist
 
 def zephyr(msg, clas='remit', instance='log', rcpt='adehnert',):
-    import os
-    os.system("zwrite -d -c '%s' -i '%s' '%s' -m '%s'" % (clas, instance, rcpt, msg, ))
+    proc = subprocess.Popen(
+        ['zwrite', '-d', '-n', '-c', clas, '-i', instance, rcpt, ],
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE
+    )
+    proc.communicate(msg)
 
 class ScriptsRemoteUserMiddleware(RemoteUserMiddleware):
     header = 'SSL_CLIENT_S_DN_Email'
